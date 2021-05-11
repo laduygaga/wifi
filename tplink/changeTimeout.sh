d339ebc28e81457be6f4928fcabba57824def94d
duynguyenngoc <hotboyarsenal@gmail.com>
2021-05-11 14:36:50 +0700

    update script change  timeout tplink

--- tplink/changeTimeout.sh
+++ tplink/changeTimeout.sh
@@ -1,3 +1,4 @@
 read -p "timeout= "  timeout
 sed -i "s/option sessiontimeout.*$/option sessiontimeout '$timeout'/g" /etc/config/nodogsplash
+/etc/init.d/nodogsplash restart
 # sed -i "s/option sessiontimeout.*$/option sessiontimeout '$timeout'/g" nodogsplash
